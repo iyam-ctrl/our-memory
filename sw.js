2c5da2e048a9c6b9ff5162442e349aa7d461ba35
iyam-ctrl <iamiay3003@gmail.com>
2026-09-11 04:24:33 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,4 @@
-
+const CACHE='ourmemory-v5';
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.json']))));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;const u=new URL(e.request.url);if(e.request.mode==='navigate'||u.origin!==location.origin)return;e.respondWith(caches.match(e.request).then(x=>x||fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match('./index.html'))));});
